1c80d3e5ee5c81126cd37ac2803763c1c8e5a3b0
maapteh <maarten@oudenniel.nl>
2020-07-17 13:30:29 +0200

    feature(401): show 401 example as well

--- codegen/generated/_graphql.tsx
+++ codegen/generated/_graphql.tsx
@@ -12,13 +12,14 @@ export type Scalars = {
   Float: number;
 };
 
-/** All available queries */
 export type Query = {
   __typename?: 'Query';
   /** Have good example which directly passes the string */
   good: Maybe<Scalars['String']>;
   /** Have a bad query which actually throws an Apollo Error */
   bad: Maybe<Scalars['String']>;
+  /**  test 401  */
+  authenticationError: Maybe<Scalars['String']>;
 };
 
 export type BadQueryVariables = Exact<{ [key: string]: never; }>;
@@ -27,6 +28,14 @@ export type BadQueryVariables = Exact<{ [key: string]: never; }>;
 export type BadQuery = (
   { __typename?: 'Query' }
   & Pick<Query, 'bad'>
+);
+
+export type AuthenticationErrorQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type AuthenticationErrorQuery = (
+  { __typename?: 'Query' }
+  & Pick<Query, 'authenticationError' | 'good'>
 );
 
 export type GoodQueryVariables = Exact<{ [key: string]: never; }>;
@@ -68,6 +77,37 @@ export function useBadLazyQuery(baseOptions?: ApolloReactHooks.LazyQueryHookOpti
 export type BadQueryHookResult = ReturnType<typeof useBadQuery>;
 export type BadLazyQueryHookResult = ReturnType<typeof useBadLazyQuery>;
 export type BadQueryResult = ApolloReactCommon.QueryResult<BadQuery, BadQueryVariables>;
+export const AuthenticationErrorDocument = gql`
+    query authenticationError {
+  authenticationError
+  good
+}
+    `;
+
+/**
+ * __useAuthenticationErrorQuery__
+ *
+ * To run a query within a React component, call `useAuthenticationErrorQuery` and pass it any options that fit your needs.
+ * When your component renders, `useAuthenticationErrorQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useAuthenticationErrorQuery({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useAuthenticationErrorQuery(baseOptions?: ApolloReactHooks.QueryHookOptions<AuthenticationErrorQuery, AuthenticationErrorQueryVariables>) {
+        return ApolloReactHooks.useQuery<AuthenticationErrorQuery, AuthenticationErrorQueryVariables>(AuthenticationErrorDocument, baseOptions);
+      }
+export function useAuthenticationErrorLazyQuery(baseOptions?: ApolloReactHooks.LazyQueryHookOptions<AuthenticationErrorQuery, AuthenticationErrorQueryVariables>) {
+          return ApolloReactHooks.useLazyQuery<AuthenticationErrorQuery, AuthenticationErrorQueryVariables>(AuthenticationErrorDocument, baseOptions);
+        }
+export type AuthenticationErrorQueryHookResult = ReturnType<typeof useAuthenticationErrorQuery>;
+export type AuthenticationErrorLazyQueryHookResult = ReturnType<typeof useAuthenticationErrorLazyQuery>;
+export type AuthenticationErrorQueryResult = ApolloReactCommon.QueryResult<AuthenticationErrorQuery, AuthenticationErrorQueryVariables>;
 export const GoodDocument = gql`
     query good {
   good
